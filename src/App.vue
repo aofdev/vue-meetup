592c94e4e8ec38cf80b777b7eef79157425b3fea
aofdev <aof.dev@gmail.com>
2017-09-11 16:05:15 +0700

    added date and time pickers

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@
         @click.stop="sideNav = !sideNav"
         class="hidden-sm-and-up "></v-toolbar-side-icon>
       <v-toolbar-title>
-        <router-link to="/" tag="span" style="cursor: pointer">DevMeetup</router-link>
+        <router-link to="/" tag="span" style="cursor: pointer">Meetup</router-link>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
